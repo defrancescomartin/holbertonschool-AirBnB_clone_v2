9de880622a4729581e6a25b12076247579ea3bb2
defrancescomartin <defrancescoma@gmail.com>
2022-08-25 18:20:38 -0300

    do deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy"""
+
+from fabric.api import local, put, env, run
+from os import path
+env.hosts = ['34.229.91.198', '	54.234.82.120']
+env.user = "ubuntu"
+
+def do_deploy(archive_path):
+    """Distributes a .tgz archive through web servers
+    """
+
+    if path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+
+        return True
+
+    return False
