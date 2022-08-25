74309d86f5182d29656c40444248db6ed0b64290
defrancescomartin <defrancescoma@gmail.com>
2022-08-25 17:10:47 -0300

    do pacj

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-# comment for the checker upto
+# Fabric script that generates a .tgz archive from
+# the contents of the web_static folder of your AirBnB Clone
+# repo, using the function do_pack
 
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """comment for the func"""
+    """Compress before sending"""
+    local("mkdir -p versions")
+    # create the name of file in str format from datetime.now
+    name = "web_static_" + datetime.strftime(datetime.now(),
+                                             "%Y%m%d%H%M%S") + ".tgz"
     try:
-        local("mkdir -p versions")
-        date = datetime.now()
-        time = date.strftime("%Y%m%d%H%M%S")
-        name = "versions/web_static_" + time + ".tgz"
-        time = "tar -cvzf " + name + " web_static"
-        local(time)
-        return name
+        local("tar -czvf ./versions/{} ./web_static" .format(name))
+        return(name)
     except:
-        return None
+        return(None)
