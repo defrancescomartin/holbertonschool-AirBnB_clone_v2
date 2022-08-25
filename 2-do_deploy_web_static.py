1e5e26acf8fa64fdd92823e4ed64c088203b3201
defrancescomartin <defrancescoma@gmail.com>
2022-08-25 18:24:14 -0300

    pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy"""
+that distributes an archive to your web servers, using the function
+do_deploy"""
 
 from fabric.api import local, put, env, run
 from os import path
 env.hosts = ['34.229.91.198', '	54.234.82.120']
 env.user = "ubuntu"
+
 
 def do_deploy(archive_path):
     """Distributes a .tgz archive through web servers
