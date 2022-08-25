d10a6dff7e716b7eb39710824693220467521ea8
defrancescomartin <defrancescoma@gmail.com>
2022-08-25 15:35:30 -0300

    web static setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
-# comment for the checker
+# Web servers set up for deployment of web static
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+#nginx installation (already installed)
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+#folder creation
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+sudo touch /data/web_static/releases/test/index.html #html creation
 echo "
 <html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current 
-chown -R ubuntu:ubuntu /data/
-sed -i "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default
-sudo service nginx restart
+ <head>
+   <title>
+   Holberton School (title)
+   </title>
+ </head>
+
+ <body>
+   Holberton School (body)
+ </body>
+ </html>" > sudo /data/web_static/releases/test/index.html
+
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current #Symlink
+
+sudo chown -R ubuntu:ubuntu /data/ #Ownership creation
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default #nginx setup
+
+sudo service nginx restart #save changes and restart the service
