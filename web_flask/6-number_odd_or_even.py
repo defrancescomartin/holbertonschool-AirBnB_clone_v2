1e60131966d80453298cefca0edf09658ad28bcd
defrancescomartin <defrancescoma@gmail.com>
2022-09-05 23:43:38 -0300

    task 6 even or odd

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Module that starts a Flask web application"""
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+ip = '0.0.0.0'
+port = 5000
+
+
+@app.route('/')
+def hello():
+    """Function that returns Hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbn():
+    """Fucntion that returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def elc(text):
+    """Function thant display C followed by text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def py_rout(text='is cool'):
+    """Returns text with python path"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def num(n):
+    """Retruns n is a number if is an int"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def num_template(n):
+    """Return an html for an int"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def even_odd(n):
+    """Return an html if a number is odd or even"""
+    if n % 2 == 0:
+        return render_template('6-number_odd_or_even.html',
+                               n="Number: {} is even".format(n))
+    else:
+        return render_template('6-number_odd_or_even.html',
+                               n="Number: {} is odd".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host=ip, port=port)
