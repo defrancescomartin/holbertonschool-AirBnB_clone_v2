4ba63e3f6c53bd4be9b33f16f87730d7d69037d7
defrancescomartin <defrancescoma@gmail.com>
2022-09-05 23:33:33 -0300

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+ip = '0.0.0.0'
+port = 5000
+
+
+@app.route('/')
+def hello():
+    """Function that returns Hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbn():
+    """Fucntion that returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def elc(text):
+    """Function thant display C followed by text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def py_rout(text='is cool'):
+    """Returns text with python path"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def num(n):
+    """Retruns n is a number if is an int"""
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host=ip, port=port)
